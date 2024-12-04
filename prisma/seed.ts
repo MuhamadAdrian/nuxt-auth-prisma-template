1966ba7baa9ed2c70d39f6ec69f5d07ebfacdd61
MuhamadAdrian <madrian.septiana@gmail.com>
2024-12-04 08:19:56 +0700

    feat: init

---
+++ prisma/seed.ts
@@ -0,0 +1,27 @@
+import prisma from "@/lib/prisma";
+import { hashPassword } from "@/server/utils/auth";
+
+async function main() {
+  const password = await hashPassword("admin123");
+
+  const admin = await prisma.user.upsert({
+    where: { email: "admin@gmail.com" },
+    update: {},
+    create: {
+      email: "admin@gmail.com",
+      name: "Admin",
+      password,
+    },
+  });
+
+  console.log({ admin });
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
